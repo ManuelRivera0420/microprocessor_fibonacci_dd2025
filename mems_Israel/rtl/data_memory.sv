c7cfc6773570cdde11c4b92cd8bf9729dbedeab3
Israelfv0109 <israelfv1280@gmail.com>
2025-11-20 16:08:08 -0600

    Se implementa con FIFO y de 1 resultado

--- mems_Israel/rtl/data_memory.sv
+++ mems_Israel/rtl/data_memory.sv
@@ -1,20 +1,21 @@
 module Data_Memory (
     input logic clk,
     input logic we,             // Write Enable
-    input logic [31:0] add,       // Address
+    input logic [31:0] a,       // Address
     input logic [31:0] wd,      // Write Data
     output logic [31:0] rd      // Read Data
 );
 
-    logic [31:0] ram [0:63]; 
+    // Memoria de 32 bits y 4 renglones
+    logic [31:0] ram [0:3]; 
 
     // Lectura combinacional
-    assign rd = ram[add[31:2]];
+    assign rd = ram[a[31:2]];
 
     // Escritura secuencial
     always_ff @(posedge clk) begin
         if (we) begin
-            ram[add[31:2]] <= wd;
+            ram[a[31:2]] <= wd;
         end
     end
 
--- mems_Israel/rtl/instruction_memory.sv
+++ mems_Israel/rtl/instruction_memory.sv
@@ -1,26 +1,34 @@
 module Instruction_Memory (
-    input  logic [31:0] add,    // Address (PC)
-    output logic [31:0] rd    // Read Data (Instrucción)
+    input logic clk,
+    
+    // Puerto de Lectura (PC)
+    input  logic [31:0] a,        // Dirección de lectura (PC)
+    output logic [31:0] rd,       // Instrucción que sale
+    
+    // Puerto de escritura (FIFO)
+    input logic [31:0] data_in,  // Dato (instruccion) que viene de la FIFO
+    input logic [31:0] dir,      // Dirección donde la FIFO quiere escribir
+    input logic        we        // Write Enable (1 = Grabar dato)
 );
 
-    logic [31:0] rom [0:63]; // Espacio para 64 instrucciones
+    // Memoria de 32 bits y 8 renglones (instrucciones)
+    logic [31:0] mem [0:7]; 
 
+    // 1. Inicialización (Limpieza)
     initial begin
-        // 1. Limpiar memoria
-        for (int i=0; i<64; i++) rom[i] = 32'b0;
-
-        // 2. Cargar programa Fibonacci (Binarios del equipo)
-        rom[0] = 32'b00000000000000000000010100010011; // ADDI x10, x0, 0              Inicializa x10 en 0
-        rom[1] = 32'b00000000000100000000010110010011; // ADDI x11, x0, 1              Inicializa x11 en 1
-        rom[2] = 32'b00000000101000000000011000010011; // ADDI x12, x0, 10             Inicializa contador x12 en 10
-        rom[3] = 32'b00000000101101010000010110110011; // ADD x11, x10, x11 (Fib)      Fibonacci: suma anterior + actual
-        rom[4] = 32'b01000000101001011000010100110011; // SUB x10, x11, x10 (Swap)     Actualiza el viejo valor usando resta
-        rom[5] = 32'b11111111111101100000011000010011; // ADDI x12, x12, -1 (Dec)      Resta 1 al contador x12
-        rom[6] = 32'b11111110000001100001101011100011; // BNE x12, x0, -12 (Loop)      Si el contador x12 no es cero, salta 3 instrucciones atrás (a la línea 4)
-        rom[7] = 32'b00000000000000000000000001101111; // JAL x0, 0 (Fin)              Fin, se queda en bucle infinito
+        for (int i=0; i<7; i++) 
+            mem[i] = 32'b0;
     end
 
-    // El PC va de 4 en 4 bytes, convertimos a indice de palabra (dividir por 4)
-    assign rd = rom[add[31:2]];
+    // 2. Escritura con la FIFO sincrona
+    always_ff @(posedge clk) begin
+        if (we) begin
+            // Discrimina los 2 bits menos significativos
+            mem[dir[31:2]] <= data_in;      // Si viene por byte (4 en4) "dir[31:2]", si viene de 1 en 1 "mem[dir]"
+        end
+    end
+
+    // 3. Lectura (Combinacional)
+    assign rd = mem[a[31:2]];               // PC lee el programa
 
 endmodule
