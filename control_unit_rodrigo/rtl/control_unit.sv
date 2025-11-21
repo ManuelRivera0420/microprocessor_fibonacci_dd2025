39ff1dca229c5c297cbe9df6ae07d29daa140e83
rodrigomatagomez <daniel.garcia@cinvestav.mx>
2025-11-21 00:50:39 +0000

    added logic for JAL instructions

--- control_unit_rodrigo/rtl/control_unit.sv
+++ control_unit_rodrigo/rtl/control_unit.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+mescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: Rodrigo Mata 
@@ -12,14 +12,14 @@
 module control_unit(
 	input logic [6:0] opcode,  //instrucctions [6:0]
 	input logic [2:0] funct_3, //instructions [14:12]
-	input logic [6:0] func_7, //instructions [31:25]
+	input logic [6:0] funct_7, //instructions [31:25]
 	output logic regwrite,
 	output logic memread,
 	output logic memwrite,
 	output logic memtoreg,
 	output logic alusrc,
 	output logic branch,
-	output logic immrd;
+	output logic [1:0] immrd,
 	output logic [3:0] alucontrol
 );
 	//Define the instructions 
@@ -31,7 +31,10 @@ module control_unit(
 	//Define the intructions for ALU
 	localparam ALU_ADD = 4'b0000;
 	localparam ALU_SUB = 4'b0001;
-	
+	//define the imm instructions 
+	localparam IMM_I = 2'b01;
+    localparam IMM_B = 2'b10;
+    localparam IMM_J = 2'b11;
     
 	always_comb begin 
 		case (opcode)
@@ -39,30 +42,30 @@ module control_unit(
 				//Type I (addi)
 				if(funct_3 == 3'b000)begin 
 					regwrite = 1'b1;
-					aluscr = 1'b1; //don't use rs2
+					alusrc = 1'b1; //don't use rs2
 					alucontrol = ALU_ADD;
-					immrd = I;
-					memread = 0;
-					memwrite = 0;
-					branch = 0;
+					immrd = IMM_I;
+					memread = '0;
+					memwrite = '0;
+					branch = '0;
 				//Type R (add)
 				end else if (funct_3 == 3'b000 && funct_7 == 7'b0000000) begin 
 					regwrite = 1'b1;
-        	aluscr = 1'b0; //use rs2
+        	alusrc = 1'b0; //use rs2
         	alucontrol = ALU_ADD;
         	immrd = '0;
-        	memread = 0;
-        	memwrite = 0;
-        	branch = 0;
+        	memread = '0;
+        	memwrite = '0;
+        	branch = '0;
 				//Type R (sub)
 				end else if (funct_3 == 3'b000 && funct_7 == 7'b0100000) begin
 					regwrite = 1'b1;
-					aluscr = 1'b0; //use rs2
+					alusrc = 1'b0; //use rs2
 					alucontrol = ALU_SUB;
 					immrd = '0;
-					memread = 0;
-					memwrite = 0;
-					branch = 0;
+					memread = '0;
+					memwrite = '0;
+					branch = '0;
  				end
 			end
 			OPCODE_B_TYPE: begin 
@@ -71,7 +74,7 @@ module control_unit(
 					regwrite = 1'b0;
 					alusrc = 1'b0; //use rs2
 					alucontrol = ALU_SUB;
-					immrd = B;
+					immrd = IMM_B;
 					memread = 1'b0;
 					memwrite = 1'b0;
 					branch = 1'b1;
@@ -81,7 +84,7 @@ module control_unit(
 				regwrite =1'b1;
 				alusrc = 1'b0;
 				alucontrol = ALU_ADD;
-				immrd = J;
+				immrd = IMM_J;
 				memread = 1'b0;
 				memwrite = 1'b0;
 				branch = 1'b1;
@@ -99,3 +102,4 @@ module control_unit(
 		endcase 
 	end
 endmodule
+
