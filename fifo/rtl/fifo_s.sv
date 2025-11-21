4bb1f8a0dab1cc0d81e2b73e8e73967ca1db4480
CbazJmz <iesebastianjimenez@gmail.com>
2025-11-20 23:20:10 -0600

    Added comments to fifo rtl file

--- fifo/rtl/fifo_s.sv
+++ fifo/rtl/fifo_s.sv
@@ -20,76 +20,53 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module fifo_s #(parameter DATA_WIDTH = 32)(
-    input logic clk,
-    input logic arst_n,
-    input logic wren,
-    input logic [DATA_WIDTH - 1:0] wdata,
-    input logic rden,
-    output logic [DATA_WIDTH - 1:0] rdata,
-    output logic full,
-    output logic empty
+    input logic clk,                            //Signal of clock
+    input logic arst_n,                         //Signal of reset
+    input logic wren,                           //Signal of write enable
+    input logic [DATA_WIDTH - 1:0] wdata,       //Paralell inputs of data to write
+    input logic rden,                           //Signal of read enable
+    output logic [DATA_WIDTH - 1:0] rdata,      //Paralell outputs of readed data
+    output logic full,                          //Signal that indicates full storange
+    output logic empty                          //Signal that indicates empty storange
 );
 
-    logic early_empty;
-    logic early_full;
-    logic [DATA_WIDTH - 1 : 0] mem [16]; // Memory declaration
-    logic [$clog2(DATA_WIDTH - 1) : 0] wrptr;
-    logic [$clog2(DATA_WIDTH - 1) : 0] rdptr;
+    logic early_empty;                          //Temporal signal that indicates the posibility of empty storange in the next iteration
+    logic early_full;                           //Temporal signal that indicates the posibility of full storange in the next iteration
+    logic [DATA_WIDTH - 1 : 0] mem [32];        //Memory declaration
+    logic [$clog2(DATA_WIDTH - 1) : 0] wrptr;   //Selector of address to write data
+    logic [$clog2(DATA_WIDTH - 1) : 0] rdptr;   //Selector of address to read data
     
     always_ff@(posedge clk, negedge arst_n) begin
-        if(!arst_n) begin
-            rdptr <= '0;
+        if(!arst_n) begin                       //Condition of reset
+            rdptr <= '0;                        //Set read address to 0
+            wrptr <= '0;                        //Set write address to 0
+            full <= 1'b0;                       //Set full to 0
+            empty <= 1'b1;                      //Set empty to 1
         end else begin
-            if(rden && (!empty))
-                rdptr <= rdptr + 1'b1;
+            if(rden && (!empty))                //Condition to read data
+                rdptr <= rdptr + 1'b1;          //Increases address of read in 1 unit
+            if(wren && (!full)) begin           //Condition to write data
+                mem[wrptr] <= wdata;            //Assignation of data in memory storange
+                wrptr <= wrptr + 1'b1;          //Increases address of write in 1 unit
+            end
+            if(early_full)                      //Condition to set full
+                full <= 1'b1;                   //Assignation of full to 1
+            if(rden)                            //Condition to set not full
+                full <= 1'b0;                   //Assignation of full to 0
+            if(early_empty)                     //Condition to set empty
+                empty <= 1'b1;                  //Asignation of empty to 1
+            if(wren)                            //Condition to set not empty
+                empty <= 1'b0;                  //Assignation of empty to 0
         end
     end
 
-    always_ff@(posedge clk, negedge arst_n) begin
-        if(!arst_n) begin
-            wrptr <= '0;
-        end else begin
-            if(wren && (!full))
-                wrptr <= wrptr + 1'b1;
-        end
-    end
+    assign rdata = rden ? mem[rdptr] : '0;                              //Assignation of readed data
+    assign early_full = (wrptr == (rdptr - 1'b1)) && wren && (!rden);   //If write address is one step to be the same than read address, and write is enable and read is not enable, then early_full is set
+    assign early_empty = (rdptr == (wrptr - 1'b1)) && rden && (!wren);  //If read address is one step to be the same than write address, and read is enable and write is not enable, then early_empty is set 
 
-    always_ff@(posedge clk) begin // Writing port
-        if(wren && (!full))
-            mem[wrptr] <= wdata;
-    end
-
-    assign rdata = rden ? mem[rdptr] : '0;
-    assign early_full = (wrptr == (rdptr - 1'b1)) && wren && (!rden);
-    assign early_empty = (rdptr == (wrptr - 1'b1)) && rden && (!wren);
-
-    always_ff@(posedge clk, negedge arst_n) begin
-        if(!arst_n) begin
-            full <= 1'b0;
-        end else begin
-            if(early_full)
-                full <= 1'b1;
-            if(rden)
-                full <= 1'b0;
-        end
-    end
-
-    always_ff@(posedge clk, negedge arst_n) begin
-        if(!arst_n) begin
-            empty <= 1'b1;
-        end else begin
-            if(early_empty)
-                empty <= 1'b1;
-            if(wren)
-                empty <= 1'b0;
-        end
-    end
-    
-    
     // Conditions to set full
     // 1.- wrptr === rdptr
     // 2.- $past(wrptr) == rdptr - 1
     // 3.- wren && (!rden)
     
-
 endmodule
