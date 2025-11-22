dc979d250f254bfcb50208d1535249b5f1f1295e
rodrigomatagomez <jose.mata@cinvestav.mx>
2025-11-22 13:54:06 -0600

    Updated the defines in order to support the intructions R, I, J, B and count in PC for normal instructions and J or B instructions also added parameters for imm generator

--- defines.svh
+++ defines.svh
@@ -1,10 +1,17 @@
-
-localparam R_TYPE  = 7'b011_0011;  	// Arithmetic and logical operations
-localparam I_LOAD  = 7'b000_0011;  	// Load data from memory
-localparam I_TYPE  = 7'b001_0011;  	// Immediate value operations
-localparam S_TYPE  = 7'b010_0011;  	// Store data in memory
-localparam B_TYPE  = 7'b110_0011;  	// Conditional branches
-localparam JAL     = 7'b110_1111;  	// Unconditional jump and link
-localparam JALR    = 7'b110_0111;  	// Jump to register location
-localparam LUI     = 7'b011_0111;  	// Load upper immediate
-localparam AUIPC   = 7'b001_0111;  	// Add upper immediate to PC
+localparam OPCODE_R_TYPE = 7'b0110011;	//Type R-Instruction
+localparam OPCODE_I_TYPE = 7'b0010011;	//Type I-Instruction
+localparam OPCODE_B_TYPE = 7'b1100011;	//Type B-Instruction
+localparam OPCODE_J_TYPE = 7'b1101111;	//Type J-Instruction
+//Define the intructions for ALU
+localparam ALU_ADD = 4'b0000;	//ALU add operation
+localparam ALU_SUB = 4'b0001;	//ALU sub operation
+//define the imm instructions 
+localparam IMM_I = 3'b000;   // Type I  (ADDI, LW, JALR, etc.)
+localparam IMM_S = 3'b001;   // Type S  (SW, SH, SB)
+localparam IMM_B = 3'b010;   // Type B  (BEQ, BNE, etc.)
+localparam IMM_U = 3'b011;   // Type U  (LUI, AUIPC)
+localparam IMM_J = 3'b100;   // Type J  (JAL)
+localparam IMM_NF = 3'b101; //No function 
+//define the PC count logic instructions 
+localparam PC_4 = 2'b00;	//Add pc + 4
+localparam PC_IMM = 2'b01; 	//Add pc + imm(value)
