4a01215aade2e791e6941962941d60d77580ad8b
ManuelRivera0420 <manuel2004rj@gmail.com>
2025-11-23 11:31:02 -0600

    Updated uart files and folders

---
+++ uart_manuel/top_communication/rtl/instruction_memory.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+module instruction_memory #(
+    parameter DATA_WIDTH = 32,      // Ancho de instruccion 
+    parameter ADDR_WIDTH = 32,      // Ancho de dirección del PC
+    parameter BYTE_WIDTH = 8,       // Ancho de cajón de la memoria (1 Byte)
+    parameter MEM_DEPTH  = 256      // Numero de renglones de memoria
+)
+(
+    input logic clk,
+    
+    // Puerto de Lectura (PC)
+    input  logic [ADDR_WIDTH-1:0] a,        // Dirección de lectura (PC 32 bits)
+    output logic [DATA_WIDTH-1:0] rd,       // Instrucción que sale
+    
+    // Puerto de escritura (FIFO)
+    input logic [DATA_WIDTH-1:0] data_in,   // Dato (instruccion) que viene de la FIFO
+    input logic [BYTE_WIDTH-1:0]            dir,       // Dirección (8 bits para 256 espacios)
+    input logic                  we         // Write Enable
+);
+
+    // Memoria de 1 byte y 256 renglones
+    logic [BYTE_WIDTH-1:0] mem [0:MEM_DEPTH-1];
+
+    // 1. Inicialización (limpieza)
+    initial begin
+        for (int i=0; i<MEM_DEPTH; i++) 
+            mem[i] = {BYTE_WIDTH{1'b0}};
+    end
+
+    // 2. Escritura con la FIFO sincrona
+    always_ff @(posedge clk) begin
+        if (we) begin
+            // Big Endian: Guardamos los 4 bytes en orden
+            mem[dir]     <= data_in[31:24];
+            mem[dir + 1] <= data_in[23:16]; 
+            mem[dir + 2] <= data_in[15:8]; 
+            mem[dir + 3] <= data_in[7:0]; 
+        end
+    end
+
+    // 3. Lectura (Combinacional)
+    assign rd = mem[a[DATA_WIDTH-1:2]];         // PC lee el programa
+
+endmodule
---
+++ uart_manuel/uart_tx_rx/rtl/receiver.sv
@@ -0,0 +1,107 @@
+
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Manuel Enrique Rivera Acosta
+// 
+// Create Date: 10/28/2025 11:54:10 AM
+// Design Name: 
+// Module Name: receiver
+//////////////////////////////////////////////////////////////////////////////////
+
+module receiver #(parameter DATA_WIDTH = 8)(
+input logic clk,
+input logic arst_n,
+input logic tick, // TICK COMING FROM THE BAUD RATE GENERATOR
+input logic rx, // INPUT SERIAL COMING FROM THE TRANSMITTER TX
+output logic rx_done, // DONE SIGNAL TO INDICATE THAT THE BYTE HAS BEEN RECEIVED
+output logic [DATA_WIDTH - 1 : 0] data_out // OUPUT SIGNAL FOR THE BYTE RECEIVED
+);
+
+localparam BIT_SAMPLING = 15;
+localparam HALFBIT_SAMPLING = 7;
+
+logic [4:0] nbits;
+logic [4:0] nbits_next;
+logic [4:0] oversampling_count;
+logic [4:0] oversampling_count_next;
+logic [DATA_WIDTH - 1 : 0] data_out_reg;
+logic [DATA_WIDTH - 1 : 0] data_out_reg_next;
+
+typedef enum logic [2:0] {IDLE = 3'b000, START = 3'b001, DATA = 3'b010, STOP = 3'b011} state_type;
+
+state_type state_reg, state_next;
+
+always_ff @(posedge clk or negedge arst_n) begin
+    if(!arst_n) begin
+        state_reg <= IDLE;
+    end else begin
+        oversampling_count <= oversampling_count_next;
+        state_reg <= state_next;
+        nbits <= nbits_next;
+        data_out_reg <= data_out_reg_next;
+    end
+end
+
+always_comb begin
+    case(state_reg)
+        IDLE: begin
+            rx_done = 1'b0;
+            oversampling_count_next = '0;
+            nbits_next = '0;
+            if(!rx) begin
+                oversampling_count_next = '0;
+                state_next = START;
+                data_out_reg_next = '0;
+            end else begin
+                state_next = IDLE;
+            end 
+        end
+        START: begin
+            if(tick) begin
+                if(oversampling_count == BIT_SAMPLING) begin
+                    state_next = DATA;
+                    oversampling_count_next = '0;
+                end else begin
+                    oversampling_count_next = oversampling_count + 1;
+                    state_next = START;
+                end
+            end else begin
+                state_next = START;
+            end
+        end
+        DATA: begin
+            if(tick) begin
+                if(oversampling_count == BIT_SAMPLING ) begin 
+                    data_out_reg_next = {rx, data_out_reg[DATA_WIDTH - 1 : 1]};
+                    oversampling_count_next = '0;
+                    if(nbits == (DATA_WIDTH - 1)) begin
+                        state_next = STOP;
+                    end else begin
+                        nbits_next = nbits + 1;
+                    end
+                end else begin
+                    oversampling_count_next = oversampling_count + 1;
+                    state_next = DATA;                    
+                end
+            end
+        end
+        STOP: begin
+            if(tick) begin
+                if(oversampling_count == BIT_SAMPLING) begin
+                    rx_done = 1'b1;
+                    state_next = IDLE;
+                end else begin
+                    oversampling_count_next = oversampling_count + 1;
+                    state_next = STOP;
+                end
+            end else begin
+                state_next = STOP;
+            end
+        end
+    endcase
+end
+
+assign data_out = data_out_reg;
+
+endmodule
