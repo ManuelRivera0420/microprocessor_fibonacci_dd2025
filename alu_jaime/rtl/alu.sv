44f657faf8b6c0e7d9a82a9f7f63a58ad71b99a6
rodrigomatagomez <jose.mata@cinvestav.mx>
2025-11-24 18:02:21 -0600

    Added changes in the logic an archiquecture of the ALU

--- alu_jaime/rtl/alu.sv
+++ alu_jaime/rtl/alu.sv
@@ -1,5 +1,4 @@
 `timescale 1ns / 1ps
-import micro_const_pkg::*;
 
 /*
  * TO DO:
@@ -14,26 +13,33 @@ import micro_const_pkg::*;
 module alu #(parameter N = 32)(
     input logic [N-1:0] reg_source1,    // First operand, sourced from a register 
     input logic [N-1:0] reg_source2,    // Second operand sourced from a register
-    input logic [N-1:0] immg_source,    // Second operand sourced from immediate generator
-    input logic alusrc,                 // ALUsrc selector for the second operand 
-    input logic [3:0]   instruction,    // Use 4 bits for the opcode at the moment 
+    //input logic [N-1:0] imm_in,         // Second operand sourced from immediate generator
+    //input logic [N-1:0] pc_in,          // First operand sourced from program counter
+    //input logic alusrc_r1,              // ALUsrc selector for the first operand
+    //input logic alusrc_r2,              // ALUsrc selector for the second operand 
+    input logic [3:0]   alucontrol,     // Use 4 bits for the opcode at the moment 
+    output logic zero,
     output logic [N-1:0] reg_destiny    // ALU result, destiny is another register
     );
-    
-    logic [N-1:0] second_operand;
+
+`include "defines.svh"    
+// internal signals
+//logic [N-1:0] first_operand;
+//logic [N-1:0] second_operand;
 
     // MUX to determine second operator; if true, use immediate value
-    assign second_operand = alusrc ? immg_source : reg_source2;
+    //assign second_operand = alusrc_r2 ? imm_in : reg_source2;
+    //assign first_operand = alusrc_r1 ? pc_in : reg_source1;
 
     always_comb begin
-        unique case(instruction)
+        unique case(alucontrol)
             ALU_ADD: begin  // At the moment we don't care about the carry
-                reg_destiny = reg_source1 + second_operand;
+                reg_destiny = reg_source1 + reg_source2;
             end
             ALU_SUB: begin
-                reg_destiny = reg_source1 - second_operand;
+                reg_destiny = reg_source1 - reg_source2;
             end
-            ALU_AND: begin
+            /*ALU_AND: begin
                 reg_destiny = reg_source1 & second_operand;
             end
             ALU_OR: begin
@@ -69,6 +75,14 @@ module alu #(parameter N = 32)(
             end
             // Send an undefined signal for non-specified OpCodes
             default: reg_destiny = 'x;
+            */
         endcase
     end
+    
+    always_comb begin
+        if (reg_destiny == 0) begin 
+            zero = 1'b1;
+        end
+    end
 endmodule
+
