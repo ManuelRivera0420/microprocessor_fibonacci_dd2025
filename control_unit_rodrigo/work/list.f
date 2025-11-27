1f22318c65c6073d51cb65141ca2827c5ad7d68a
DanielGG50 <daniel.garcia@cinvestav.mx>
2025-11-27 02:24:58 +0000

    Changed the RTL in order to test and created xcelium

--- control_unit_rodrigo/work/list.f
+++ control_unit_rodrigo/work/list.f
@@ -1,3 +1,15 @@
 # ============================ rtl
+../rtl/defines.svh
+../rtl/microprocessor.sv
+../rtl/instruction_memory.sv
+../rtl/bank_reg_s.sv
+../rtl/alu.sv
+../rtl/program_counter.sv
+../rtl/imm_gen.sv
+../rtl/mux.sv
 ../rtl/control_unit.sv
+
 # ============================ tb
+
+../verif/microprocessor_tb.sv
+
