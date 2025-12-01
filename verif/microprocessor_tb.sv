4675743fedcf68b70f6ba176dfa04524552ff6a3
DanielGG50 <daniel.garcia@cinvestav.mx>
2025-12-01 20:58:51 +0000

    change tb

--- verif/microprocessor_tb.sv
+++ verif/microprocessor_tb.sv
@@ -1,15 +1,19 @@
 module microprocessor_tb ();
-
-	bit clk;
-	bit arst_n;
-	logic [DATA_WIDTH - 1:0] instruction;
-
-	always #5ns clk = !clk;
+    bit clk;
+    bit arst_n;
+    logic [DIR_WIDTH-1:0] rd;
+    logic [DIR_WIDTH-1:0] rs1;
+    logic [DIR_WIDTH-1:0] rs2;
+    logic [12:0] imm_bq;
+    logic [20:0] imm_jal;
+    logic [DATA_WIDTH - 1:0] instruction;
+    
+    always #5ns clk = !clk;
 	assign #10ns arst_n = 1'b1;
 
-	microprocessor_if uprocessor_if(clk, arst_n);
-
-	`define  MEM_PATH microprocessor_i.instruction_memory_i
+    microprocessor_if tbprocessor_if(clk, arst_n);   
+    
+    `define  MEM_PATH microprocessor_i.instruction_memory_i
 	`define  ALU_PATH microprocessor_i.alu_i
 	`define  BANK_REG_PATH microprocessor_i.prf_i
 	`define  PC_PATH microprocessor_i.pc_i
@@ -17,28 +21,51 @@ module microprocessor_tb ();
 	`define  MUX_IMM_PATH microprocessor_i.mux_imm_gen_i
 	`define  MUX_PC_PATH microprocessor_i.mux_pc_i
 	`define  CU_PATH microprocessor_i.control_unit_i
-
-	initial begin 
+    
+    initial begin
     wait (arst_n);
-    //instruction = 32'b0000_0000_0000_0000_0000_0101_0001_0011; //RS1 = 0,  RD = 10, addi
-		uprocessor_if.write_addi_instr(5'b01010, 5'b00000);
-		#10ns;
-    $display("RESULT = %d", `ALU_PATH.alu_result);
-		$display("opcode = %d", `CU_PATH.opcode);
-	end
+     
+        repeat(10)begin 
+            std::randomize(rd,rs1);
+            instruction = tbprocessor_if.write_addi_instr(rd,rs1);
+            #10ns;
+//            $display("RESULT = %d", `ALU_PATH.alu_result);
+//		    $display("opcode = %d", `CU_PATH.opcode);
+        end
+                
+        repeat(10)begin 
+            std::randomize(rs1,rs2,rd);
+            instruction = tbprocessor_if.write_add_instr(rs1,rs2,rd);
+            #10ns;
+//            $display("RESULT = %d", `ALU_PATH.alu_result);
+//		    $display("opcode = %d", `CU_PATH.opcode);
+        end
+        
+        repeat(10)begin 
+            std::randomize(rs1,rs2);
+            instruction = tbprocessor_if.write_beq_instr(rs1,rs2, std::randomize);
+            #10ns;
+//            $display("RESULT = %d", `ALU_PATH.alu_result);
+//		    $display("opcode = %d", `CU_PATH.opcode);
+        end
+        
+        repeat(10)begin 
+            std::randomize(rd);
+            instruction = tbprocessor_if.write_jal_instr (rd, std::randomize);
+            #10ns;
+//            $display("RESULT = %d", `ALU_PATH.alu_result);
+//		    $display("opcode = %d", `CU_PATH.opcode);
+        end
+        
+    end 
 
-	always_comb begin
-		instruction = uprocessor_if.instruction;
-	end
-
-	microprocessor_top microprocessor_i (
-    .clk(clk),
-    .arst_n(arst_n),
-    .instruction(uprocessor_if.instruction)
-	);
-
-
-	initial begin // Initial block to open shared memory and probe signals
+    microprocessor_top microprocessor_i (
+        .clk(clk),
+        .arst_n(arst_n),
+        .instruction(tbprocessor_if.instruction)
+        );
+        
+    initial begin // Initial block to open shared memory and probe signals
 			$shm_open("shm_db");
 			$shm_probe("ASMTR");
 	end
@@ -47,58 +74,5 @@ module microprocessor_tb ();
 		#100us;
 		$finish;
 	end
-
+    
 endmodule
-
-
-/*
-module microprocessor_tb ();
-
-//`include "defines.svh"
-
-`define  MEM_PATH microprocessor_i.instruction_memory_i
-`define  ALU_PATH microprocessor_i.alu_i
-`define  BANK_REG_PATH microprocessor_i.prf_i
-`define  PC_PATH microprocessor_i.pc_i
-`define  IMM_GEN_PATH microprocessor_i.imm_gen_i
-`define  MUX_IMM_PATH microprocessor_i.mux_imm_gen_i
-`define  MUX_PC_PATH microprocessor_i.mux_pc_i
-`define  CU_PATH microprocessor_i.control_unit_i
-
-
-bit clk;
-bit arst_n;
-logic [DATA_WIDTH - 1:0] instruction;
-
-always #5ns clk = !clk;
-assign #10ns arst_n = 1'b1;
-
-initial begin 
-    wait (arst_n);
-    instruction = 32'b00000000000000000000010100010011;
-		#10ms;
-    $display("RESULT = %d", `ALU_PATH.alu_result);
-		$display("RESULT = %d", `CU_PATH.opcode);
-		$finish;
-end
-
-
-
-
-
-microprocessor microprocessor_i (
-    .clk(clk),
-    .arst_n(arst_n),
-    .instruction(instruction)
-);
-
-
-	initial begin // Initial block to open shared memory and probe signals
-			$shm_open("shm_db");
-			$shm_probe("ASMTR");
-	end
-
-
-
- endmodule
-*/
